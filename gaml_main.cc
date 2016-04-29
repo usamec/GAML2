c0500c1c4ca50b85b7fc156f2e8470eab583862d
usamec <usama@ksp.sk>
2016-04-29 22:33:10 +0200

    something useful

--- gaml_main.cc
+++ gaml_main.cc
@@ -2,6 +2,7 @@
 #include "path.h"
 #include "read_set.h"
 #include "read_probability_calculator.h"
+#include "moves.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
@@ -21,8 +22,26 @@ int main(int argc, char** argv) {
 
   SingleReadProbabilityCalculator pc(&rs, 0.01, -10, -0.7, 0, 0);
   ProbabilityChange prob_change;
-  cout << pc.GetPathsProbability(paths, prob_change) << endl;
+  double old_prob = pc.GetPathsProbability(paths, prob_change);
+  cout << old_prob << endl;
+  pc.ApplyProbabilityChange(prob_change);
 
   ofstream of(argv[3]);
   PathsToFasta(paths, of);
+
+  cout << PathsToDebugString(paths) << endl;
+  MoveConfig config;
+  for (int i = 0; i < 2; i++) {
+    cout << "Iter: " << i << endl;
+    vector<Path> new_paths;
+    MakeMove(paths, new_paths, config);
+    double new_prob = pc.GetPathsProbability(new_paths, prob_change);
+    cout << new_prob << endl;
+    cout << PathsToDebugString(new_paths) << endl << endl;
+    if (new_prob > old_prob) {
+      old_prob = new_prob;
+      paths = new_paths;
+      pc.ApplyProbabilityChange(prob_change);
+    }
+  }
 }
